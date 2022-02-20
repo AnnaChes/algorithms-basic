a5b92b015d32773af41dd3a22c812fc1f8e45534
Ann <78258873+AnnaChes@users.noreply.github.com>
2022-02-20 19:53:10 +0500

    :0

--- algorithms/getQueryParamsString.test.js
+++ algorithms/getQueryParamsString.test.js
@@ -1,24 +1,19 @@
 describe('getQueryParamsString.test', () => {
-  it('test getQueryParamsString.test', () => {
- 
-    
+    it('test getQueryParamsString.test', () => {
+        const obj1 = {name:"hello", showSize:7, gender:"W"}//name=hello&showSize=7&gender=W
 
+        const getQueryParamsString = (obj) => {
+           let res = ''
+           for (let name in obj) {
+            res = res + name + '=' + obj[name] + '&'
+           }
+           return res.slice(0, -1)
+        }
+        
 
-    const getQueryParamsString = (obj) => {
-        return 'name' + '=' + obj.name + '&' + 'showSize' +  '=' + obj.showSize + '&'+  'gender'+ '='  + obj.gender
-    }
-  
-  
-
-
-    let obj1 = {name:"hello", showSize:7, gender:"W"}//name=hello&shoeSize=7&gender=W
-    let output = getQueryParamsString(obj1)
-    const expected = 'name=hello&shoeSize=7&gender=W'
-    expect(output).toEqual(expected)
-    
-
-
-
-
+        const output = getQueryParamsString(obj1)
+        const expected = 'name=hello&showSize=7&gender=W'
+        expect(output).toEqual(expected)
+    })
   })
-})
+  
